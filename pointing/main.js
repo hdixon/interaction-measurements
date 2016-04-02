bb5990849aba9659ba0e1abaf81e6c79c82626ba
hdixon <hdixon@cmu.edu>
2016-04-02 19:58:46 -0400

    we now have circle of circles

--- pointing/main.js
+++ pointing/main.js
@@ -1,13 +1,24 @@
+init();
+
 function init() {
     drawCircles();
 }
 
-function onFrame(event) {
-    // drawCircles();
-}
+// function onFrame(event) {
+//     drawCircles();
+// }
+
 
 function drawCircles() {
-    drawCircle(100, 100);
+    var pi = Math.PI;
+    var cx = view.bounds.width / 2;
+    var cy = view.bounds.height / 2;
+
+    for (var i = 0; i < pi * 2; i += pi / 5) {
+        var x = cx + Math.cos(i) * 150;
+        var y = cy + Math.sin(i) * 150;
+        drawCircle(x, y);
+    }
 }
 
 function drawCircle(x, y) {

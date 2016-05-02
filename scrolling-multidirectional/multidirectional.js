4b446e34f1f0772aa738d5dee71ae75b69a48fef
hdixon <hdixon@cmu.edu>
2016-05-02 04:29:57 -0400

    add arrow pointAt function

--- scrolling-multidirectional/multidirectional.js
+++ scrolling-multidirectional/multidirectional.js
@@ -11,10 +11,24 @@ var targetContainer = new Shape.Circle(view.center.x, view.center.y, 100);
 targetContainer.fillColor = 'rgba(2, 2, 2, 0.4)';
 
 
-
 function changeCenter(oldCenter, deltaX, deltaY, factor) {
    var offset;
    offset = new paper.Point(deltaX, -deltaY);
    offset = offset.multiply(factor);
    return oldCenter.add(offset);
  }
+
+
+function pointAt(x1, y1, x2, y2) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var angle = Math.atan2(dy, dx);
+
+    var x = Math.cos(angle);
+    var y = Math.sin(angle);
+    console.log(x);
+    console.log(y);
+    console.log(x + y);
+
+    return [x, y];
+}
